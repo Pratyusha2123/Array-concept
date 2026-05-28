7c5558c6ebd9b20b99b246894eb793abb1ff83b7
Pratyusha Dhal <pratyusshaofficial@gmail.com>
2026-05-28 21:22:02 +0530

    array program
    five array programs

---
+++ inout.java
@@ -0,0 +1,13 @@
+public class inout {
+    public static void main(String[] args) {
+
+        String sub = "sub5";
+        int index = 3;
+        String output = "output12354";
+
+        System.out.println("Sub: " + sub);
+        System.out.println("Index: " + index);
+        System.out.println("Output: " + output);
+    }
+}
+
---
+++ shifthalfarray.java
@@ -0,0 +1,18 @@
+public class shifthalfarray {
+    public static void main(String[] args) {
+
+        int[] arr = {2, 5, 8, 9, 3, 1};
+
+        int mid = arr.length / 2;
+
+        System.out.print("Output: ");
+
+        for (int i = mid; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        for (int i = 0; i < mid; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
