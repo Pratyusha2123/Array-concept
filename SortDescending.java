a7d90f5b36164f75c59af37e6913f0326b78d7f0
Pratyusha Dhal <pratyusshaofficial@gmail.com>
2026-06-26 20:17:31 +0530

    Added SortArray and SortDescending programs

---
+++ SortDescending.java
@@ -0,0 +1,27 @@
+public class SortDescending {
+
+    public static void sortDescending(int[] arr) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.print("Sorted Array: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    public static void main(String[] args) {
+
+        int[] arr = {9, 5, 6, 1, 2, 7};
+
+        sortDescending(arr);
+    }
+}
